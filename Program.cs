e19e5e72ff9c434504236acd4b2c3665927c9a92
Tyler Tenny <tyler.tenny@oit.edu>
2026-06-02 00:17:13 -0700

    migrate in code

--- Program.cs
+++ Program.cs
@@ -55,6 +55,8 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+    db.Database.Migrate();
+
     if (db.Users.Count() <= 2)
     {
         db.Users.Add(new User
